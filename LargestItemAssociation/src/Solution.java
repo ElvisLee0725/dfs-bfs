8c4f3f63d59b33fb5d69732bf2f6c424cf7df41d
ElvisLee <elvislee0725@gmail.com>
2021-05-21 17:12:02 -0700

    Add Union Find solution

--- LargestItemAssociation/src/Solution.java
+++ LargestItemAssociation/src/Solution.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        List<PairString> ia = new ArrayList(Arrays.asList(new PairString("item1", "item2"), new PairString("item6", "item1"), new PairString("item3", "item4"), new PairString("item4", "item5"), new PairString("item4", "item2")));
+        List<PairString> ia = new ArrayList(Arrays.asList(new PairString("item1", "item2"), new PairString("item6", "item1"), new PairString("item3", "item4"), new PairString("item4", "item5")));
 
-        System.out.println(new Solution().LargestItemAssociation(ia));
+        System.out.println(new Solution().LargestItemAssociation2(ia));
     }
     // Use a HashMap to create the adjacency list for all items
     // To make the adjacency list's key sorted, use TreeMap
@@ -43,5 +43,63 @@ public class Solution {
                 dfs(nei, tmp, map, hs);
             }
         }
+    }
+
+    // Union Find:
+    // Use a hashmap with <Item Name, Root of this item>, and another hashmap <Item Name, Weight>
+    // Iterate the input itemAssociation List, find the root of each pair
+    // If the root of that pair doesn't match, do union and record the largest union
+    // Return the set with largest union
+    List<String> LargestItemAssociation2(List<PairString> itemAssociation) {
+        TreeMap<String, String> map = new TreeMap();
+        HashMap<String, Integer> weightMap = new HashMap();
+        String tmp = "";
+        int max = 0;
+        for(PairString ps : itemAssociation) {
+            String item1 = ps.first;
+            String item2 = ps.second;
+            map.putIfAbsent(item1, item1);
+            map.putIfAbsent(item2, item2);
+            String r1 = find(item1, map);
+            String r2 = find(item2, map);
+
+            if (!r1.equals(r2)) {
+                weightMap.putIfAbsent(r1, 1);
+                weightMap.putIfAbsent(r2, 1);
+                if (r1.compareTo(r2) < 0) {
+                    map.put(r2, r1);
+                    weightMap.put(r1, weightMap.get(r1) + weightMap.get(r2));
+                    if (weightMap.get(r1) > max || (max == weightMap.get(r1) && r1.compareTo(tmp) < 0)) {
+                        max = weightMap.get(r1);
+                        tmp = r1;
+                    }
+                } else {
+                    map.put(r1, r2);
+                    weightMap.put(r2, weightMap.get(r2) + weightMap.get(r1));
+                    if (weightMap.get(r2) > max || (max == weightMap.get(r2) && r2.compareTo(tmp) < 0)) {
+                        max = weightMap.get(r2);
+                        tmp = r2;
+                    }
+                }
+            }
+        }
+        List<String> res = new ArrayList();
+        HashSet<String> hs = new HashSet();
+        hs.add(tmp);
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (hs.contains(entry.getValue())) {
+                hs.add(entry.getKey());
+                res.add(entry.getKey());
+            }
+        }
+        return res;
+    }
+
+    private String find(String x, TreeMap<String, String> map) {
+        if(map.get(x).equals(x)) {
+            return x;
+        }
+        map.put(x, find(map.get(x), map));
+        return map.get(x);
     }
 }
